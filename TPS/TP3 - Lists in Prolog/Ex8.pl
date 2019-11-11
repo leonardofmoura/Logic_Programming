91e5363f7ecbe8ab13a4cfdb1f46e4ae2e52ccee
leonardofmoura <up201706907@fe.up.pt>
2019-11-11 18:44:40 +0000

    Added new list length function using accumulators

--- TPS/TP3 - Lists in Prolog/Ex8.pl
+++ TPS/TP3 - Lists in Prolog/Ex8.pl
@@ -6,6 +6,16 @@ count([_|T],N) :-
     count(T,Dec),  
     N is Dec+1.
 
+
+% OR USING ACCUMULATOR
+count_acc(List,N) :- count_acc0(List,0,N).
+
+count_acc0([],Acc,Acc).
+
+count_acc0([_|T],Acc,Len) :-
+    NAcc is Acc + 1,
+    count_acc0(T,NAcc,Len).
+
 % b)
 % count_elem(X,List,N) -> True if X ocurrs N times in List
 count_elem(_,[],0).
